ded3eb340865061589df0a7e579d762c10c369e2
Santiago <santiperez1199@gmail.com>
2021-01-07 20:40:48 +0100

    creating script lightingCalices

---
+++ Assets/_SenetVR/Scripts/LightingCalices.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingCalices : MonoBehaviour
+{
+
+    public List<GameObject> calices;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(waitSeconds(1));
+        LightCalices2();
+    }
+
+    private IEnumerator waitSeconds(int seconds)
+    {
+        LightCalices();
+        yield return new WaitForSeconds(seconds);
+    }
+
+    private IEnumerator waitSeconds2(int seconds)
+    {
+        LightCalices2();
+        yield return new WaitForSeconds(seconds);
+    }
+
+    private void LightCalices()
+    {
+        calices[0].transform.GetChild(1).gameObject.SetActive(true);
+        calices[1].transform.GetChild(1).gameObject.SetActive(true);
+
+
+
+    }
+
+    private void LightCalices2()
+    {
+        calices[2].transform.GetChild(1).gameObject.SetActive(true);
+        calices[3].transform.GetChild(1).gameObject.SetActive(true);
+
+
+
+    }
+}
