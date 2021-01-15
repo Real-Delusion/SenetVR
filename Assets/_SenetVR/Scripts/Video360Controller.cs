d2b377afa56ff4ab92430e7bb717943984557133
Rosa <emiliarosavdh@gmail.com>
2021-01-15 12:46:34 +0100

    Go back to main screen when video finishes

---
+++ Assets/_SenetVR/Scripts/Video360Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video360Controller : MonoBehaviour {
+
+    public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start () {
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached (UnityEngine.Video.VideoPlayer vp) {
+        GameManager.instance.levelManager.goMainScreen ();
+    }
+}
