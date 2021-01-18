5ad4d98b74bb177c98efd8d3ac7551682f3425f6
Santiago <santiperez1199@gmail.com>
2021-01-18 18:20:49 +0100

    Lighting caliz with torch finished

---
+++ Assets/_SenetVR/Scripts/LightCalizWithTorch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCalizWithTorch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Flame")
+        {
+            this.transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+}
