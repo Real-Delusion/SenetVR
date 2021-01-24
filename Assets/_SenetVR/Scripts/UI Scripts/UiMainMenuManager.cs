61e7d64c39f0cb3fe3403ba69d097ca2cb646316
Rosa <emiliarosavdh@gmail.com>
2021-01-24 23:38:11 +0100

    Open mainscene after video finishes playing

--- Assets/_SenetVR/Scripts/UI Scripts/UiMainMenuManager.cs
+++ Assets/_SenetVR/Scripts/UI Scripts/UiMainMenuManager.cs
@@ -2,38 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UiMainMenuManager : MonoBehaviour
-{
+public class UiMainMenuManager : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 
-    public void GoToMainMenu()
-    {
-        GameManager.instance.levelManager.goMainScreen();
+    public void GoToMainMenu () {
+        GameManager.instance.levelManager.goMainScreen ();
     }
 
-    public void GoToTemple1() 
-    { 
-        GameManager.LoadSceneAsync("Temple1");
+    public void GoToTemple1 () {
+        GameManager.LoadSceneAsync ("Temple1");
     }
 
-    public void ExitGame()
-    {
-        GameManager.instance.levelManager.ExitGame();
+    public void ExitGame () {
+        GameManager.instance.levelManager.ExitGame ();
     }
 
-    public void ChargeVideo360()
-    {
-
+    public void ChargeVideo360 () {
+        GameManager.LoadSceneAsync ("Video360");
     }
 
     public void GoFallas()
