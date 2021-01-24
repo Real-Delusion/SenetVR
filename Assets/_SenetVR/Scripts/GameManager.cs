1268cd2c2c024b0655871aae66b1211cee435730
Rosa <emiliarosavdh@gmail.com>
2021-01-25 00:53:54 +0100

    Adding all scenes to build settings

--- Assets/_SenetVR/Scripts/GameManager.cs
+++ Assets/_SenetVR/Scripts/GameManager.cs
@@ -94,7 +94,7 @@ public class GameManager : MonoBehaviour
         string scenePrefix = "Temple";
 
         // Add
-        for (int i = 2; i < 7; i++)
+        for (int i = 2; i <= 11; i++)
         {
             string scene = scenePrefix + i;
             
