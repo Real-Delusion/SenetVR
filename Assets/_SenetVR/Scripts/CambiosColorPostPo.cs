987c623241ecfd5fb3be9132cf3b2f638f86b173
almiab1 <almiab1@epsg.upv.es>
2021-01-25 02:10:07 +0100

    init change color

---
+++ Assets/_SenetVR/Scripts/CambiosColorPostPo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class CambiosColorPostPo : MonoBehaviour
+{
+    // --------------------------------------------------------------------------
+    // Scripts
+    // --------------------------------------------------------------------------
+    [Header("Scripts")]
+    [SerializeField]
+    private Volume _volumPostPorcesingVolume;
+
+    // --------------------------------------------------------------------------
+    // Vars
+    // --------------------------------------------------------------------------
+    private ColorAdjustments _colorAdjustments;
+
+    // --------------------------------------------------------------------------
+    // Runetime
+    // --------------------------------------------------------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volumPostPorcesingVolume = GetComponent<Volume>();
+
+        _volumPostPorcesingVolume.profile.TryGet(out _colorAdjustments);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
